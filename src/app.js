e1c09b1ed1bce93d44a19665303da9890a390d56
Eva Mothapo <matlalaeva6@gmail.com>
2025-10-07 19:38:06 +0200

    Injected the icon data

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -28,8 +31,8 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  if (minutes<10){
-    minutes=`0${minutes}`
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
   return `${day} ${hours}:${minutes}, `;
 }
